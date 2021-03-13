d7974186f03e286f2bd8f13078345ebfc0edabaa
Unsik6 <80208196+Unsik6@users.noreply.github.com>
2021-03-13 14:11:59 +0900

    03-13 UI

---
+++ Hunter_4Masters/Assets/Scripts/Joystick.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+// 터치 처리를 위한 인터페이스 상속
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image joystickArea;
+    private Image joystick;
+    private Vector2 inputVector;    //이동 벡터값
+
+    void Start()
+    {
+        joystickArea = GetComponent<Image>();
+        joystick = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    // IDragHandler 인터페이스에 선언된 OnDrag 메소드 구현
+    // 터치중일 동안 조이스틱 이동 및 이동벡터값 저장
+    public void OnDrag(PointerEventData ped)
+    {   
+        Vector2 pos;
+        // 터치 시작 위치가 조이스틱 배경 이내이면 pos에 로컬 좌표를 저장
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickArea.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            // pixel 단위로 되어있는 pos 값을 조이스틱 배경크기에 대한 비율(-1 ~ 1)로 변환
+            pos.x = (pos.x / joystickArea.rectTransform.sizeDelta.x)*2;
+            pos.y = (pos.y / joystickArea.rectTransform.sizeDelta.y)*2;
+
+            inputVector = pos;
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            // 조이스틱을 앵커(=조이스틱 배경의 중심)를 기준으로 터치한 곳으로 이동
+            joystick.rectTransform.anchoredPosition = new Vector2(inputVector.x * (joystickArea.rectTransform.sizeDelta.x * 0.4f),
+                inputVector.y * (joystickArea.rectTransform.sizeDelta.y * 0.4f));
+        }
+    }
+
+    // IPointerUpHandler 인터페이스에 선언된 OnPointerDown 메소드 구현
+    // 터치 시작 시 호출
+    public void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+
+    // IPointerDownHandler 인터페이스에 선언된 OnPointerUp 메소드 구현
+    // 뗐을 때 조이스틱 위치 리셋
+    public void OnPointerUp(PointerEventData ped)
+    {
+        inputVector = Vector2.zero;
+        joystick.rectTransform.anchoredPosition = Vector2.zero;
+    }
+
+    // x값 반환(플레이어 스크립트에서 사용)
+    public float GetHorizontalValue()
+    {
+        return inputVector.x;
+    }
+
+    // y값 반환(이 게임에서는 사용하지 않음)
+    //public float GetVerticalValue()
+    //{
+    //    return inputVector.y;
+    //}
+}
---
+++ Hunter_4Masters/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Joystick joystick;
+    public float moveSpeed;
+    public float jumpForce;
+    public float jumpSpeed;
+    private bool isJumping = false;
+    private SpriteRenderer sr;
+
+    public GameObject attackMsg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        attackMsg.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false;
+    }
+
+    public void Move()
+    {
+        Vector3 offset = new Vector3(joystick.GetHorizontalValue() * moveSpeed * Time.deltaTime, 0, 0);
+        // 이동방향에 맞춰 플레이어 스프라이트 뒤집기(정지상태 제외)
+        if (offset != Vector3.zero)
+        {
+            sr.flipX = !(offset.x < 0);
+        }
+
+        // 화면 밖 넘어가지 않도록 이동제한
+        Vector3 newViewPos = Camera.main.WorldToViewportPoint(this.transform.position + offset);
+        newViewPos.x = Mathf.Clamp01(newViewPos.x);
+        this.transform.position = Camera.main.ViewportToWorldPoint(newViewPos);
+    }
+
+    public void Jump()
+    {
+        if (!isJumping)
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce);
+            isJumping = true;
+        }
+    }
+
+    public void Attack()
+    {
+        //공격 애니메이션 설정
+        //아직 공격애니메이션 없어서 공격 애니메이션 대신 넣었음
+        StartCoroutine(AttackCoroutine());
+    }
+
+    IEnumerator AttackCoroutine()
+    {
+        attackMsg.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        attackMsg.SetActive(false);
+    }
+}
