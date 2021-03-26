0c88c9c907d8f2297e065318f8445629004ca0dc
hyerinlee <herring027@gmail.com>
2021-03-26 20:46:14 +0900

    Change UI size and position
    크기 및 위치 수정
    대상: 시뮬레이션 팝업창(선택/실행), 배경, 거주지 이동 콜라이더

--- Hunter_4Masters/Assets/01_Scripts/03_AreaScene/AreaMove.cs
+++ Hunter_4Masters/Assets/01_Scripts/03_AreaScene/AreaMove.cs
@@ -12,6 +12,8 @@ public class AreaMove : MonoBehaviour
 
     private void Start()
     {
+        // 콜라이더 좌표를 해상도에 맞게 지정(오른쪽 가장자리)
+        this.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(1.03f, 0.6f, 0));
         sp = selectPanel.GetComponent<SelectionPopup>();
     }
 
