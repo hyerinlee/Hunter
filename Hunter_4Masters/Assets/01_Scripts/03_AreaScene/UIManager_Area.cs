ee8c59e81fb57076f9e04cc085208e8a3184d10e
hyerinlee <herring027@gmail.com>
2021-06-04 18:55:47 +0900

    Improve cons-etc UI showing
    - Connect to player data
    - Add temporary (hardcoded) data and sprites

--- Hunter_4Masters/Assets/01_Scripts/03_AreaScene/UIManager_Area.cs
+++ Hunter_4Masters/Assets/01_Scripts/03_AreaScene/UIManager_Area.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class UIManager_Area : Singleton<UIManager_Area>
 {
     [SerializeField] private Image hpGauge, spGauge;
-    [SerializeField] private GameObject interaction, dialogBox;
+    [SerializeField] private GameObject interaction, dialogBox, etcPanel, etcDetail;
     [SerializeField] private Popup popup;
     [SerializeField] public Text hp, sp, day, time, money;
+    private List<GameObject> etcList = new List<GameObject>();
     private PlayerData pd;
+    private List<ConsEtc> etc = new List<ConsEtc>();
 
     public override void Awake()
     {
-        
+        for (int i = 0; i < etcPanel.transform.childCount; i++)
+        {
+            etcList.Add(etcPanel.transform.GetChild(i).gameObject);
+        }
     }
 
     void Start()
@@ -26,6 +32,24 @@ public class UIManager_Area : Singleton<UIManager_Area>
         if (GameManager.Instance.isPlay) SetUI();
     }
 
+    private void SetEtcUI()
+    {
+        for(int i=0; i<etcList.Count; i++)
+        {
+            etcList[i].GetComponent<Button>().onClick.RemoveAllListeners();
+            etcList[i].SetActive(false);
+        }
+        for (int i = 0; i < pd.Cons.ETC.Count; i++)
+        {
+            etcList[i].SetActive(true);
+            etcList[i].transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load("icons/" + pd.Cons.ETC[i].name, typeof(Sprite)) as Sprite;
+            int imgIndex = i;
+            int etcIndex = pd.Cons.ETC[i].index;
+            etcList[i].GetComponent<Button>().onClick.AddListener(() => { StopAllCoroutines(); StartCoroutine(ShowEtcDetailCoroutine(imgIndex, etcIndex)); });
+        }
+        etc = pd.Cons.ETC.ToList();
+    }
+
     public void SetUI()
     {
         pd = FosterManager.Instance.GetPlayerData();
@@ -36,6 +60,7 @@ public class UIManager_Area : Singleton<UIManager_Area>
         money.text = pd.GetMoney();
         day.text = GameManager.Instance.GetDDay();
         time.text = GameManager.Instance.GetCurrentTimeByValue();
+        if (etc.Count==0 || etc.Intersect(pd.Cons.ETC).Count()!=etc.Count) SetEtcUI();
     }
 
     public void ActiveInteraction(Vector2 pos, string simulName)
@@ -54,5 +79,14 @@ public class UIManager_Area : Singleton<UIManager_Area>
     {
         interaction.GetComponent<Button>().onClick.RemoveAllListeners();
         interaction.SetActive(false);
+    }
+
+    private IEnumerator ShowEtcDetailCoroutine(int imgIndex, int etcIndex)
+    {
+        etcDetail.SetActive(true);
+        etcDetail.GetComponent<Image>().rectTransform.anchoredPosition = etcPanel.transform.GetChild(imgIndex).GetComponent<Image>().rectTransform.anchoredPosition + new Vector2(150,-150);
+        etcDetail.transform.GetChild(0).GetComponent<Text>().text = DataManager.Instance.GetEtcDetail(etcIndex);
+        yield return new WaitForSeconds(2f);
+        etcDetail.SetActive(false);
     }
 }
